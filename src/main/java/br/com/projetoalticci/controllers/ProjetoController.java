a64170f85040c13377449a596b732cbd19efdabf
MateusFS99 <mateusfsantos.pfs@gmail.com>
2022-06-28 21:50:04 -0300

    - Project created; Alticci function done;

---
+++ src/main/java/br/com/projetoalticci/controllers/ProjetoController.java
@@ -0,0 +1,34 @@
+package br.com.projetoalticci.controllers;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ProjetoController {
+
+    private int alticci(int n) {
+        if(n < 2) 
+            return n;
+        else if(n == 2)
+            return 1;
+        else
+            return alticci(n - 3) + alticci(n - 2);
+    }
+
+    @GetMapping("/alticci/{n}")
+    public ResponseEntity<List<Integer>> mapAlticci(@PathVariable(value = "n") int n) {
+
+        List<Integer> result = new ArrayList<>();
+
+        for (int i = 0; i < n; i++) 
+            result.add(alticci(i));
+
+        return new ResponseEntity<List<Integer>>(result, HttpStatus.OK);
+    }
+}
